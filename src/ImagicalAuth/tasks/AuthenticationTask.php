8533a24f43098342673277fdaa3c823400970f72
xBeastMode <xBeastMode@users.noreply.github.com>
2016-04-06 16:51:39 -0700

    Create AuthenticationTask.php

---
+++ src/ImagicalAuth/tasks/AuthenticationTask.php
@@ -0,0 +1,30 @@
+<?php
+namespace imagicalmine\tasks;
+use ImagicalAuth\Main;
+use pocketmine\Player;
+use pocketmine\scheduler\PluginTask;
+class AuthenticationTask extends PluginTask{
+    /** @var Main */
+    private $plugin;
+    /** @var Player */
+    private $player;
+    /**
+     * @param Main $plugin
+     * @param Player $p
+     */
+    public function __construct(Main $plugin, Player $p){
+        parent::__construct($plugin);
+        $this->plugin = $plugin;
+        $this->player = $p;
+    }
+    /**
+     * Actions to execute when run
+     *
+     * @param $currentTick
+     *
+     * @return void
+     */
+    public function onRun($currentTick){
+        $this->player->kick($this->plugin->getMessage("authentication_delay_kick_reason"));
+    }
+}
