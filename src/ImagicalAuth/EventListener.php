4fd21b44ee9d7af17b13b757d01d597dba929612
xBeastMode <xBeastMode@users.noreply.github.com>
2016-04-06 16:50:30 -0700

    Update EventListener.php

--- src/ImagicalAuth/EventListener.php
+++ src/ImagicalAuth/EventListener.php
@@ -1,27 +1,19 @@
 <?php
-
-  namespace ImagicalAuth;
-
-  use pocketmine\plugin\PluginBase;
-  use pocketmine\event\Listener;
-  use pocketmine\utils\TextFormat as TF;
-  use pocketmine\event\player\PlayerJoinEvent;
-
-  class EventListener extends PluginBase implements Listener
-  {
-
-    public function __construct(Main $plugin)
-    {
-
+namespace imagicalmine;
+use pocketmine\plugin\PluginBase;
+use pocketmine\event\Listener;
+use pocketmine\event\player\PlayerJoinEvent;
+class EventListener extends PluginBase implements Listener{
+  /** @var Main */
+  private $plugin;
+  /**
+   * @param Main $plugin
+   */
+    public function __construct(Main $plugin){
       $this->plugin = $plugin;
-
     }
 
-    public function onJoin(PlayerJoinEvent $event)
-    {
+    public function onJoin(PlayerJoinEvent $event){
 
     }
-
-  }
-
-?>
+}
